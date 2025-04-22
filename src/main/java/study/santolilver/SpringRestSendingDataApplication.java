9ccd37dab96bdd2eb5282173fc1143a73c9dc5cc
Nathália Oliveira <nathaliaolisantana@gmail.com>
2025-04-22 12:13:45 -0300

    first commit and publishing the project

---
+++ src/main/java/study/santolilver/SpringRestSendingDataApplication.java
@@ -0,0 +1,13 @@
+package study.santolilver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestSendingDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestSendingDataApplication.class, args);
+	}
+
+}
